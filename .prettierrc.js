6d943ddc8cf75a147f110bd3d14d3aa02039e4d3
Oleksii Zahoruiko <zahoruiko.oleksii@gmail.com>
2023-09-13 11:47:18 +0200

    Init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+};
---
+++ styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export const COLORS = {
+  dark: 'black',
+  light: 'white'
+};
+
+const baseContainerStyles = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+const baseBoxStyles = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderWidth: 2,
+  height: '90%',
+  width: '90%',
+};
+
+const lightStyleSheet = StyleSheet.create({
+  container: {
+    ...baseContainerStyles,
+    backgroundColor: COLORS.light
+  },
+  box: {
+    ...baseBoxStyles,
+    borderColor: COLORS.dark
+  }
+});
+
+const darkStyleSheet = StyleSheet.create({
+  container: {
+    ...baseContainerStyles,
+    backgroundColor: COLORS.dark
+  },
+  box: {
+    ...baseBoxStyles,
+    borderColor: COLORS.light
+  }
+});
+
+export default function getStyleSheet(useDarkTheme) {
+  return useDarkTheme ? darkStyleSheet : lightStyleSheet;
+};
